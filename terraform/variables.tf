7ecaae21d19b18afe7e498dd34c97d771b1e191a
Israel Herraiz <ihr@google.com>
2023-01-20 18:03:10 +0100

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "billing_account" {
+  description = "Billing account for the projects/resources"
+  type        = string
+  default = null
+}
+
+variable "create_project" {
+  description = "Set to false if your project already exists"
+  type        = bool
+  default     = false
+}
+
+variable "project_id" {
+  description = "Id for the project to be created"
+  type        = string
+}
+
+variable "region" {
+  description = "The region for resources and networking"
+  type        = string
+}
+
+
